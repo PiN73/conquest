7ee637e401c647a8d5a199219ed75e279d0c9a06
PiN73 <p97p@ya.ru>
2017-02-19 22:04:11 +0300

    added class Unit and its rendering

--- main/Game.java
+++ main/Game.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+
 public class Game {
   Map map;
+  ArrayList<MyUnit> myUnits;
+  ArrayList<Enemy> enemies;
   
-  public Game(String mapFileName) {
+  Game(String mapFileName) {
     map = new Map(mapFileName);
+    myUnits = new ArrayList<MyUnit>();
+    enemies = new ArrayList<Enemy>();
+    
+    for(int y = 0; y < map.h; ++y)
+      for(int x = 0; x < map.w; ++x)
+        if (map.cell_type(x, y) == 1)  
+          myUnits.add(new MyUnit(x, y));
+        else if (map.cell_type(x, y) == 2)
+          enemies.add(new Enemy(x, y));
   }
 }
